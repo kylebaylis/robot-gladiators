37f2d9d2a78146c5f99c9152ca8a369bd2a91f64
Kyle <kylejmbaylis@gmail.com>
2022-03-08 14:30:07 -0500

    var, function, and console log

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,22 @@
-window.alert("This is an alert! JavaScript is running!");
+// this creates a function named "fight"
+
+var playerName = window.prompt("What is your robot's name?");
+
+// What is this? It outputs a log when using inspect in the browser and going to console, tells you what line of your code was used
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+
+// this will do math and log 20
+console.log(10 + 10);
+
+// adding a string plus the var (user input)
+console.log("Our robot's name is " + playerName);
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// this calls the function named "fight"
+
+// fight();
